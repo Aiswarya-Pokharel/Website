97f16f4109f0f48ce2dc59d37e5b09609df36446
Aiswarya Pokharel <pokharelaishwarya22@gmail.com>
2024-12-21 23:05:22 +0545

    updates

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const nav = document.getElementById("nav");
-const bar = document.querySelector('.bar');
-function menu(){
+const nav = document.getElementById("navbar");
+const bar = document.querySelector(".bar");
+const closeBtn = document.getElementById("cross");
+
+function menu() {
+  nav.classList.add("active");
+  bar.style.display = "none";
   nav.style.display = "block";
-  bar.style.display = "none";   
 }
-function closeMenu(){
-  nav.style.display = "none";
-  bar.style.display = "block";   
+
+function closeMenu() {
+  nav.classList.remove("active");
+  bar.style.display = "block";
 }
-const form= document.querySelector('form');
-function sendEmail(){
+
+bar.addEventListener("click", menu);
+closeBtn.addEventListener("click", closeMenu);
+
+const form = document.querySelector("form");
+function sendEmail() {
   const name = document.getElementById("Name").value;
   const email = document.getElementById("Email").value;
   const msg = document.getElementById("Msg").value;
 
-
-  let body = "Name: " + name + "<br/> Email: " + email + "<br/> Message: " + msg;
+  let body =
+    "Name: " + name + "<br/> Email: " + email + "<br/> Message: " + msg;
   Email.send({
-    SecureToken : "7f496236-4743-4409-9d94-58c53d2da661",
-    To : 'pokharelaishwarya22@gmail.com',
-    From : "pokharelaishwarya22@gmail.com",
-    Subject : "message from Contact from",
-    Body : body,
-}).then(
-  message => alert(message)
-);
+    SecureToken: "7f496236-4743-4409-9d94-58c53d2da661",
+    To: "pokharelaishwarya22@gmail.com",
+    From: "pokharelaishwarya22@gmail.com",
+    Subject: "message from Contact from",
+    Body: body,
+  }).then((message) => alert(message));
 }
